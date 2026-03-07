af189a6392bb18e1641adfde583e4a8ed28daceb
SantiagoAMD <santiago.gomez.castro.04@gmail.com>
2026-03-07 13:49:29 -0500

    Code completed

--- challenge-04/src/services/auth.ts
+++ challenge-04/src/services/auth.ts
@@ -5,12 +5,11 @@ export const isLoggedIn = (): boolean => {
 };
 
 export const login = (email: string, password: string): boolean => {
-  // Credenciales del PDF
   const ok = email === "user@mail.com" && password === "123";
-  if (ok) localStorage.setItem(LOGGED_KEY, "true"); // token logged=true :contentReference[oaicite:6]{index=6}
+  if (ok) localStorage.setItem(LOGGED_KEY, "true"); 
   return ok;
 };
 
 export const logout = () => {
-  localStorage.removeItem(LOGGED_KEY); // limpiar token :contentReference[oaicite:7]{index=7}
+  localStorage.removeItem(LOGGED_KEY); 
 };
